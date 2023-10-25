1f2f4c2a375be80b92c83a2f4bf7a4fd1e891495
Xianzhang Chen <83766153+xianzhangchen@users.noreply.github.com>
2023-10-25 15:51:31 +0800

    oct
    oct

--- examples/1d/RKKY/main.py
+++ examples/1d/RKKY/main.py
@@ -2,19 +2,24 @@
 import os ; import sys 
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
-from pyqula import geometry
-g = geometry.chain() # linear chain
-ns = 1 # number of supercells
-g = g.get_supercell(ns)
-h = g.get_hamiltonian() # get the Hamiltonian
-from pyqula import rkky
-m = rkky.rkky_map(h,n=10//ns,nk=100,mode="LR")
+
 import numpy as np
-np.savetxt("RKKY.OUT",m)
+from pyqula import geometry
+g = geometry.chain()
+h = g.get_hamiltonian() # create hamiltonian of the system
+
+h.add_onsite(1.)
+from pyqula import rkky
+out = rkky.rkky_map(h,n=10,mode="pm",fsuper=10,nk=1).T
+x0,e0 = out[0],out[3]
+
+out1 = rkky.rkky_map(h,n=10,mode="LR",nk=200).T
+x1,e1 = out1[0],out1[3]
 
 import matplotlib.pyplot as plt
-plt.plot(m[:,0],m[:,3],marker="o")
 
+plt.plot(x0,e0,marker="o")
+plt.plot(x1,e1,marker="o")
 plt.show()
 
 
